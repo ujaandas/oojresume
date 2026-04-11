fbebee3ced4a0fe0ed8d99e41237ce4363cb2843
Ujaan Das <ujaandas03@gmail.com>
2026-04-11 22:35:50 +0000

    add config, use json to specify opts

---
+++ config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+/*
+Define the fields we can modify (or are expected to be modified) in the template files.
+
+All templates are referenced by template name (ie; "edu_hkust" or "proj_dissertation" as
+defined in their respective .tmpl files)
+*/
+
+type Identity struct {
+	Name     string
+	Email    string
+	Phone    string
+	LinkedIn string
+	Github   string
+	Website  string
+}
+
+type EducationTmplName string
+
+type ExperienceTmplName string
+
+type ProjectTmplName string
+
+type SkillsetTmplName string
+
+type AwardTemplName string
+
+type Config struct {
+	Identity   Identity
+	Education  []EducationTmplName
+	Experience []ExperienceTmplName
+	Project    []ProjectTmplName
+	Skills     SkillsetTmplName
+	Awards     []AwardTemplName
+}
+
+func getConfig(dir, cfgFilename string) (Config, error) {
+	cfg := Config{}
+
+	cfgFile, err := os.Open(dir + cfgFilename)
+	if err != nil {
+		return Config{}, err
+	}
+	defer cfgFile.Close()
+
+	jsonParser := json.NewDecoder(cfgFile)
+	jsonParser.Decode(&cfg)
+
+	return cfg, nil
+}
