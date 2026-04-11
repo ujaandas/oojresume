293b660441a6a60d9bdbd70526d13532e3086ef9
Ujaan Das <ujaandas03@gmail.com>
2026-04-11 22:45:05 +0000

    add main cli flags

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+type Config struct {
+	dir string
+}
+
+func loadAppCfg() Config {
+	var cfg Config
+	flag.StringVar(&cfg.dir, "dir", ".", "set directory to look for config file in")
+	flag.Parse()
+	return cfg
+}
 
 func main() {
-	fmt.Println("hello world!")
+	cfg := loadAppCfg()
+	fmt.Println("config dir:", cfg.dir)
 }
